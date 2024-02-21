b037a4889a61aaba22a9431516028b9e94a4d3ca
Muhammad Naveed <149371965+naveed-devOps@users.noreply.github.com>
2024-02-21 11:58:17 +0500

    update file

--- discord-backend/app.js
+++ discord-backend/app.js
@@ -8,12 +8,7 @@ import cors from 'cors';
 
 const MONGO_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT || 5000;  
-
- 
-
-
-
-
+  
 const app = express();
 app.use(cors());
 app.use(express.json());
